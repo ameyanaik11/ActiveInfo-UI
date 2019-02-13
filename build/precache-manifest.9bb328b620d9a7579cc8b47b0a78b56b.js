41840a7e59510145443189c4c331625fd81cbb67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-13 08:34:27 +0000

    Deploy ameyanaik11/ActiveInfo-UI to github.com/ameyanaik11/ActiveInfo-UI.git:gh-pages

---
+++ build/precache-manifest.9bb328b620d9a7579cc8b47b0a78b56b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc395144419c0130d57",
+    "url": "/static/css/main.7fc4ec50.chunk.css"
+  },
+  {
+    "revision": "dcc395144419c0130d57",
+    "url": "/static/js/main.dcc39514.chunk.js"
+  },
+  {
+    "revision": "b9106835a0ab4182dc6c",
+    "url": "/static/js/1.b9106835.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25525606b467b428f649e9b9ae18fb28",
+    "url": "/index.html"
+  }
+];
