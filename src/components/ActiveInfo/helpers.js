59aa569d8980e74e0105d2c5171d7233964a6e76
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-13 08:39:05 +0000

    Deploy ameyanaik11/ActiveInfo-UI to github.com/ameyanaik11/ActiveInfo-UI.git:gh-pages

--- src/components/ActiveInfo/helpers.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-
-export const iftttDate = iftttTimestamp =>
-  moment(iftttTimestamp, "MMMM DD, YYYY at LT"); // January 11, 2019 at 03:27PM
-
-export const iftttDateOrUndefined = iftttTimestamp => {
-  const parsedDate = iftttDate(iftttTimestamp);
-  return parsedDate.isValid() ? parsedDate : undefined;
-};
