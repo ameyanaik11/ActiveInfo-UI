02544a9368f6a7a8748b072a8d6896e62f2f2290
Ameya Naik <anaik@twitter.com>
2019-02-15 09:46:13 -0800

    Updates

---
+++ precache-manifest.ad1c3d8a134ff5a781cfc394b5d14e85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "904d6dacad36aae47a15",
+    "url": "/ActiveInfo-UI/static/js/runtime~main.904d6dac.js"
+  },
+  {
+    "revision": "cc27bf71aa315ddc30e6",
+    "url": "/ActiveInfo-UI/static/js/main.cc27bf71.chunk.js"
+  },
+  {
+    "revision": "1afa91e7567309974f85",
+    "url": "/ActiveInfo-UI/static/js/1.1afa91e7.chunk.js"
+  },
+  {
+    "revision": "cc27bf71aa315ddc30e6",
+    "url": "/ActiveInfo-UI/static/css/main.a31c8c6d.chunk.css"
+  },
+  {
+    "revision": "89e839495c08de424a8eaf409ea3ab1f",
+    "url": "/ActiveInfo-UI/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bb328b620d9a7579cc8b47b0a78b56b.js"
+  "/ActiveInfo-UI/precache-manifest.ad1c3d8a134ff5a781cfc394b5d14e85.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ActiveInfo-UI/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
